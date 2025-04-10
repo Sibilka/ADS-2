8a92b45721335a994fdfc2c886fd62ab7bd1fff6
Sibilka <barsik05072005@gmail.com>
2025-04-10 19:51:30 +0300

    2 ads

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,26 +3,57 @@
 #include "alg.h"
 
 
-double pown(double value, uint16_t n) {
-  return 0.0;
+bool checkPrime(uint64_t value) {
+  if (value < 2) return false;
+  for (uint64_t i = 2; i * i <= value; i++) {
+    if (value % i == 0) return false;
+  }
+  return true;
 }
 
-uint64_t fact(uint16_t n) {
-  return 0.0;
+uint64_t nPrime(uint64_t n) {
+  uint64_t kol = 0;
+  uint64_t num = 0;
+
+  if (n < 2) return 0;
+  while (kol < n) {
+    num++;
+    if (checkPrime(num)) {
+      kol++;
+    }
+  }
+  return num;
 }
 
-double calcItem(double x, uint16_t n) {
-  return 0.0;
+uint64_t nextPrime(uint64_t value) {
+  value++;
+  while (!checkPrime(value)) {
+    value++;
+  }
+  return value;
 }
 
-double expn(double x, uint16_t count) {
-  return 0.0;
+uint64_t sumPrime(uint64_t hbound) {
+  uint64_t sum = 0;
+  for (uint64_t num = 2; num < hbound; num++) {
+    if (checkPrime(num)) {
+      sum += num;
+    }
+  }
+  return sum;
 }
 
-double sinn(double x, uint16_t count) {
-  return 0.0;
-}
+uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
+  uint64_t kol = 0;
+  uint64_t pervChisl = 0;
 
-double cosn(double x, uint16_t count) {
-  return 0.0;
+  for (uint64_t num = lbound; num < hbound; num++) {
+    if (checkPrime(num)) {
+      if (pervChisl != 0 && num - pervChisl == 2) {
+        kol++;
+      }
+      pervChisl = num;
+    }
+  }
+  return kol;
 }
