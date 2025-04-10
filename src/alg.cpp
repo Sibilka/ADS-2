7b10dc74875fa6d5748a57cbd39f82b1c96cc1e0
Sibilka <barsik05072005@gmail.com>
2025-04-10 20:02:04 +0300

    3 ads

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,48 @@
-// Copyright 2022 NNTU-CS
-#include <cstdint>
-#include "alg.h"
+cpp
+// Copyright 2025 NNTU-CS
+🟥 include <cstdint>
+🟥 include "alg.h"
 
-
-double pown(double value, uint16_t n) {
-  return 0.0;
+uint64t collatzMaxValue(uint64t startingNum) {
+  uint64_t maximumValue = startingNum;
+  while (startingNum != 1) {
+    if (startingNum % 2 == 0) {
+      startingNum /= 2;
+    } else {
+      startingNum = 3 * startingNum + 1;
+    }
+    if (startingNum > maximumValue) {
+      maximumValue = startingNum;
+    }
+  }
+  return maximumValue;
 }
 
-uint64_t fact(uint16_t n) {
-  return 0.0;
+unsigned int collatzLength(uint64_t startingNum) {
+  unsigned int length = 1;
+  while (startingNum != 1) {
+    if (startingNum % 2 == 0) {
+      startingNum /= 2;
+    } else {
+      startingNum = 3 * startingNum + 1;
+    }
+    length++;
+  }
+  return length;
 }
 
-double calcItem(double x, uint16_t n) {
-  return 0.0;
-}
-
-double expn(double x, uint16_t count) {
-  return 0.0;
-}
-
-double sinn(double x, uint16_t count) {
-  return 0.0;
-}
-
-double cosn(double x, uint16_t count) {
-  return 0.0;
+unsigned int findLongestCollatzSeq(unsigned int *maxLength,
+                                    uint64_t lowerBound,
+                                    uint64_t upperBound) {
+  unsigned int longestLength = 0;
+  unsigned int numberWithLongestLength = lowerBound;
+  for (uint64_t i = lowerBound; i <= upperBound; ++i) {
+    unsigned int length = collatzLength(i);
+    if (length > longestLength) {
+      longestLength = length;
+      numberWithLongestLength = i;
+    }
+  }
+  *maxLength = longestLength;
+  return numberWithLongestLength;
 }
